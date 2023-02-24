195ef863c0d03b2fb2a7c55d8817ef6032f0c49f
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-02-24 09:44:40 -0800

    WIP trying to figure out how to create method test with correct variables.

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -12,5 +12,13 @@ namespace Bakery.Tests
       Bread newBread = new Bread();
       Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
+
+    [TestMethod]
+    public void BreadOrder_ReturnsPriceForBread_Int()
+    {
+      int input = 1;
+      int result = (input);
+      Assert.AreEqual(5, result);
+    }
   }
 }
