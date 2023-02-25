793286dc532720d0fa445855fe08e3cf3484ee86
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-02-24 17:22:50 -0800

    Add missing semicolons to Program.cs

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -10,10 +10,10 @@ namespace Bakery
     {
       Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
       Console.WriteLine("Welcome to Pierre's French Bakery!");
-      Console.WriteLine("Each loaf of bread costs $5, but today's deal is buy 2, get 1 free!")
+      Console.WriteLine("Each loaf of bread costs $5, but today's deal is buy 2, get 1 free!");
       Console.WriteLine("How many loaves of bread would you like?");
       string stringNumber1 = Console.ReadLine();
-      Console.WriteLine("Each pastry costs $2, but today they are on sale for buy 3, get 1 free!")
+      Console.WriteLine("Each pastry costs $2, but today they are on sale for buy 3, get 1 free!");
       Console.WriteLine("How many pastries would you like?");
       string stringNumber2 = Console.ReadLine();
       // code to calculate bread price + pastry price
